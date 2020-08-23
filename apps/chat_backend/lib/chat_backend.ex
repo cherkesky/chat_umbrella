813429c052e19fefab56016cac957119c04d80ea
Guy Cherkesky <guy@cherkesky.com>
2020-08-23 15:31:33 -0500

    mix new chat_backend --module ChatBackend --sup

---
+++ apps/chat_backend/lib/chat_backend.ex
@@ -0,0 +1,18 @@
+defmodule ChatBackend do
+  @moduledoc """
+  Documentation for `ChatBackend`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChatBackend.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
